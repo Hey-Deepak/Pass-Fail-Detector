4e08d899bd47724ba385e07afab2bdc89729744c
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-01 00:45:48 +0530

    updated

--- myScript.js
+++ myScript.js
@@ -1,6 +1,16 @@
 function myFunction(){
+    var myInput = prompt("Enter Your Name :-");
     document.getElementById("demo").innerHTML = " LOL.."
+    
+    document.getElementById("p01").innerHTML = myInput;
+
+    document.getElementById('display').innerHTML = 
+                    document.getElementById("user_input").value;
 }
+
+function showMessage(){
+    var message = document.getElementById("message").value;
+    display_message.innerHTML= message;
 
 
 document.getElementById("p01").innerHTML = "Enter your First In-Sem Marks:-";
