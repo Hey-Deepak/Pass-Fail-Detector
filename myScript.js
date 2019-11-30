755742864abc2dcc03d7b2d1ee5a067581fd2383
Deepak Choudhary <choudharydeepak990@gmail.com>
2019-12-01 03:52:45 +0530

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,16 +1,50 @@
-function myFunction(){
-    var myInput = prompt("Enter Your Name :-");
-    document.getElementById("demo").innerHTML = " LOL.."
+// function myFunction(){
+//     var myInput = prompt("Enter Your Name :-");
+//     document.getElementById("demo").innerHTML = " LOL.."
     
-    document.getElementById("p01").innerHTML = myInput;
+//     document.getElementById("p01").innerHTML = myInput;
 
-    document.getElementById('display').innerHTML = 
-                    document.getElementById("user_input").value;
-}
-
-function showMessage(){
-    var message = document.getElementById("message").value;
-    display_message.innerHTML= message;
+//     document.getElementById('display').innerHTML = 
+//                     document.getElementById("user_input").value;
+// }
 
 
-document.getElementById("p01").innerHTML = "Enter your First In-Sem Marks:-";
+//         function showInput() {
+//             document.getElementById('display1').innerHTML = document.getElementById("user_input1").value;
+//             document.getElementById('display2').innerHTML = document.getElementById("user_input2").value;
+//             document.getElementById('display3').innerHTML = document.getElementById("user_input3").value;
+//             document.getElementById('display4').innerHTML = document.getElementById("user_input4").value;
+//             document.getElementById('display5').innerHTML = document.getElementById("user_input5").value;
+//         }
+
+//         function myResult(){
+//             var firstInSem = document.getElementById("user_input1").value; ; 
+//             var secondInSem = document.getElementById("user_input2").value;; 
+//             var finalSem = document.getElementById("user_input3").value;
+//             var assignment= document.getElementById("user_input4").value; 
+//             var attendance = document.getElementById("user_input5").value;;
+//             var total;
+//             total = (firstInSem*30)/100 + (secondInSem*30)/100 + (finalSem*50)/100 + (assignment) + (attendance);
+   
+//             document.getElementById("display").innerHTML = "Your Total Marks :-" + total;
+//     if (total >= 35)
+//     {
+//         <p>"You are Safe and you crossed Danger Zone, Your extra marks is " + total - 35 </p>
+       
+//     } else
+//         <p>"You are UnSafe and you have to gain " + (35 - total)*2 + "more marks.\n Means Next time in Final Back Paper you have to score " +(finalSem + (35 - total)*2) </p>
+//     }
+ 
+//         // document.getElementById("p01").innerHTML = "Enter your First In-Sem Marks:-" ; 
+//         // document.getElementById("p02").innerHTML = "Enter your Second In-Sem Marks:-";
+//         // document.getElementById("p03").innerHTML = "Enter your Final-Sem Marks:-";
+//         // document.getElementById("p04").innerHTML = "Enter your Assignment Marks:-";
+//         // document.getElementById("p05").innerHTML = "Enter your Attendance Marks:-";
+
+
+// function showMessage(){
+//     var message = document.getElementById("message").value;
+//     display_message.innerHTML= message;
+
+
+// document.getElementById("p01").innerHTML = "Enter your First In-Sem Marks:-";
